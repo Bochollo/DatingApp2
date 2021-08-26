10610778c9675ac6514d0c616f14e0a4ba8d8963
Bochollo <gerhard.rogg@web.de>
2021-08-26 21:46:03 +0200

    Login added to API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,10 +37,34 @@ namespace API.Controllers
 
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            //using var hmac = new HMACSHA512();
+            //hmac.Key = user.PasswordSalt;
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++) 
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
+
+
 
     }
 }
